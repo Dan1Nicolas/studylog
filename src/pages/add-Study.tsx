d8725a05b3aeedfcd47bedc19295db09997e231b
CarolinaaFontes <acfonts1@gmail.com>
2025-09-25 16:31:04 -0300

    feat: add useStates

--- src/pages/add-Study.tsx
+++ src/pages/add-Study.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import type { StudySession } from "../types/StudySession"; 
+
 export function AddStudy() {
+     const [studies, setStudies] = useState<StudySession[]>([]);
+
   return (
     <>
       <h2>Adicionar nova sessão de estudos</h2>
-      <p>Total de tarefas:</p>
+       <p>Total de tarefas: {studies.length}</p>
       <p>Tempo de tarefas:</p>
     </>
   );

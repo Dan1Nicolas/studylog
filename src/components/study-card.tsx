28f97a3377622697c64e4a39e66b34cb34c1d3b3
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-09-24 22:46:47 -0300

    feat: criando os study card

---
+++ src/components/study-card.tsx
@@ -0,0 +1,17 @@
+import type { StudySession } from "../types/study";
+
+interface StudyCardProps {
+    study: StudySession
+}
+
+export default function StudyCard({ study }: StudyCardProps) {
+    return (
+        <div>
+        
+            <p>{study.subject}</p>
+            <p>Minutos: {study.minutes} min</p>
+            <p>Data: {study.date}</p>
+            <p>Notas: {study.notes}</p>
+        </div>
+    )
+}

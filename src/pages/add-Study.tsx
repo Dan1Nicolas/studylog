9b63141ddb76c7a137c5bad93e087f8b44a9805b
CarolinaaFontes <acfonts1@gmail.com>
2025-09-25 23:15:04 -0300

    feat: adicionando useCallback no add-Study

--- src/pages/add-Study.tsx
+++ src/pages/add-Study.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { StudyForm } from "../components/Study-form";
 import type { StudySession } from "../types/study";
 
@@ -9,13 +10,20 @@ interface AddStudyProps {
 export function AddStudy({ sessions, onAddSession }: AddStudyProps) {
   const totalMinutes = sessions.reduce((sum, s) => sum + s.minutes, 0);
 
+  const handleAddSession = useCallback(
+    (session: StudySession) => {
+      onAddSession(session);
+    },
+    [onAddSession]
+  );
+
   return (
     <div className="max-w-2xl mx-auto p-6">
       <h2 className="text-2xl font-bold mb-4 flex justify-center items-center">Adicionar nova sessão de estudos</h2>
       <p className="flex justify-center items-center font-medium p-1">Total de sessões: {sessions.length}</p>
       <p className="flex justify-center items-center font-medium p-1">Total de minutos estudados: {totalMinutes}</p>
 
-      <StudyForm onAddSession={onAddSession} />
+      <StudyForm onAddSession={handleAddSession} />
 
       {sessions.length > 0 && (
         <ul className="mt-4 border border-gray-500 p-2 rounded-md font-medium">

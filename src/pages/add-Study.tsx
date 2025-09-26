d8a632afa2b98381c988a7d88e232e30f5aafe0e
CarolinaaFontes <acfonts1@gmail.com>
2025-09-25 22:23:59 -0300

    feat: add style with Tailwind all pages

--- src/pages/add-Study.tsx
+++ src/pages/add-Study.tsx
@@ -11,14 +11,14 @@ export function AddStudy({ sessions, onAddSession }: AddStudyProps) {
 
   return (
     <div className="max-w-2xl mx-auto p-6">
-      <h2 className="text-2xl font-bold mb-4">Adicionar nova sessão de estudos</h2>
-      <p>Total de sessões: {sessions.length}</p>
-      <p>Total de minutos estudados: {totalMinutes}</p>
+      <h2 className="text-2xl font-bold mb-4 flex justify-center items-center">Adicionar nova sessão de estudos</h2>
+      <p className="flex justify-center items-center font-medium p-1">Total de sessões: {sessions.length}</p>
+      <p className="flex justify-center items-center font-medium p-1">Total de minutos estudados: {totalMinutes}</p>
 
       <StudyForm onAddSession={onAddSession} />
 
       {sessions.length > 0 && (
-        <ul className="mt-4">
+        <ul className="mt-4 border border-gray-500 p-2 rounded-md font-medium">
           {sessions.map((s) => (
             <li key={s.id}>
               {s.subject} — {s.minutes} min em {new Date(s.date).toLocaleDateString("pt-BR")}
